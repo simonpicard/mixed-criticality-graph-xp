02d68f6775222bfb2fc94f60bd3f79386d6087e3
simonpicard <simonpicardgithub@gmail.com>
2024-01-03 01:41:22 +0100

    edge case

--- src/cpp/UnsafeOracle.cpp
+++ src/cpp/UnsafeOracle.cpp
@@ -22,29 +22,27 @@ bool UnsafeOracle::interference(State* state) {
         if (job->is_active()) {
             int ttd = job->get_ttd();
             int total_rct = job->get_rct();
-            bool forecast_one_step_ahead = false;
+            bool all_other_idle = false;
             for (int j = 0; j < state->get_jobs().size(); j++) {
                 if (i == j) continue;
                 Job* other_job = state->get_jobs()[j];
                 if (other_job->is_discarded(state->get_crit())) continue;
                 int other_ttd = other_job->get_ttd();
+                all_other_idle |= !other_job->is_active();
                 if (other_ttd <= ttd) {
                     total_rct += other_job->get_rct();
                     other_ttd += other_job->get_T();
-                }
-                if (other_ttd <= ttd and not forecast_one_step_ahead) {
-                    total_rct -=
-                        1;  // in this situation, we are going to look at what
-                            // jobs will be emited at the next transition, thus
-                            // we need to integrate that a job will be executed
-                    forecast_one_step_ahead = true;
                 }
                 while (other_ttd <= ttd) {
                     total_rct += other_job->get_C()[state->get_crit() - 1];
                     other_ttd += other_job->get_T();
                 }
-                if (total_rct > ttd) return true;
             }
+            if (all_other_idle) total_rct -= 1;
+            // if all other jobs are idle, then the job will be executed before
+            // other jobs can emit and increase the load TODO check this for
+            // other cases
+            if (total_rct > ttd) return true;
         }
     }
 
@@ -61,12 +59,13 @@ bool UnsafeOracle::interference_at_level(State* state, int crit) {
             int total_rct =
                 job->get_rct() +
                 (job->get_C()[crit - 1] - job->get_C()[state->get_crit() - 1]);
-            bool forecast_one_step_ahead = false;
+            bool all_other_idle = false;
             for (int j = 0; j < state->get_jobs().size(); j++) {
                 if (i == j) continue;
                 Job* other_job = state->get_jobs()[j];
                 if (other_job->is_discarded(crit)) continue;
                 int other_ttd = other_job->get_ttd();
+                all_other_idle |= !other_job->is_active();
                 if (other_ttd <= ttd) {
                     if (other_job->is_active())
                         total_rct +=
@@ -75,16 +74,13 @@ bool UnsafeOracle::interference_at_level(State* state, int crit) {
                              other_job->get_C()[state->get_crit() - 1]);
                     other_ttd += other_job->get_T();
                 }
-                if (other_ttd <= ttd and not forecast_one_step_ahead) {
-                    total_rct -= 1;
-                    forecast_one_step_ahead = true;
-                }
                 while (other_ttd <= ttd) {
                     total_rct += other_job->get_C()[crit - 1];
                     other_ttd += other_job->get_T();
                 }
-                if (total_rct > ttd) return true;
             }
+            if (all_other_idle) total_rct -= 1;
+            if (total_rct > ttd) return true;
         }
     }
 
