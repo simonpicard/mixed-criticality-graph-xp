0d72c56bdcc889f4fbe2d8f6ea5cfd5e6bf24b9b
Antonio Paolillo <apaolill@gmail.com>
2024-02-06 18:43:53 +0100

    Add unsafe oracle based on sum of sorted laxities
    Signed-off-by: Antonio Paolillo <apaolill@gmail.com>

--- mcsexplorer/edf_safe.cpp
+++ mcsexplorer/edf_safe.cpp
@@ -1,6 +1,6 @@
 #include "EdfInterference.h"
+#include "UnsafeOracle.h"
 #include "State.h"
-#include "Types.h"
 
 int main(int argc, char** argv) {
     std::vector<Job*> jobs = {
@@ -12,8 +12,10 @@ int main(int argc, char** argv) {
     State state = State(jobs);
 
     const bool is_unsafe = unsafe_edf_system_interference(&state);
-
     std::cout << "EDF is " << (is_unsafe ? "unsafe" : "not unsafe") << " on that system." << std::endl;
+
+    const bool sum_min_lax_unsafe = UnsafeOracle::sum_sorted_laxities(&state);
+    std::cout << "sum_min_lax oracle says " << (sum_min_lax_unsafe ? "unsafe" : "not unsafe") << " on that system." << std::endl;
 
     return 0;
 }
