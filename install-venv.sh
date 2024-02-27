439c3a958ec3795ab7c2ec9c171b16b3e0d840cf
simonpicard <simonpicardgithub@gmail.com>
2024-02-27 20:01:52 +0100

    macos compatibility

--- install-venv.sh
+++ install-venv.sh
@@ -1,20 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
-script_dir=$(readlink -e "$(dirname "$0")")
-root_dir=$(readlink -e "${script_dir}")
+# Define script directory, root directory, and venv directory
+script_dir=$(dirname "$0")
+root_dir=$(cd "$script_dir" && pwd)
 venv_dir=$1
 
-if [ -z "${venv_dir}" ]
-then
-  echo "Please provide the target venv directory on command line." >&2
+# Check if the venv directory is provided as an argument
+if [ -z "$venv_dir" ]; then
+  echo "Please provide the target venv directory on the command line." >&2
   exit 1
 fi
 
-PYTHON=python3
-VENV=${venv_dir}
-PIP=${VENV}/bin/pip
+PYTHON=python3.11
+VENV="$venv_dir"
+PIP="$VENV/bin/pip"
 
-${PYTHON} -m venv "${VENV}"
-${PIP} install --upgrade pip
-${PIP} install --requirement "${root_dir}/requirements.txt"
+# Create a virtual environment, upgrade pip, and install requirements
+$PYTHON -m venv "$VENV"
+$PIP install --upgrade pip
+$PIP install -r "${root_dir}/requirements.txt"
