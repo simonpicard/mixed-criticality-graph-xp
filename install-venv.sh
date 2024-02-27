b0dc0201293e121103a2b8e6ae906c08ef2932be
simonpicard <simonpicardgithub@gmail.com>
2024-02-27 23:05:26 +0100

    greadlink

--- install-venv.sh
+++ install-venv.sh
@@ -1,22 +1,20 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-# Define script directory, root directory, and venv directory
-script_dir=$(dirname "$0")
-root_dir=$(cd "$script_dir" && pwd)
+script_dir=$(greadlink -e "$(dirname "$0")")
+root_dir=$(greadlink -e "${script_dir}")
 venv_dir=$1
 
-# Check if the venv directory is provided as an argument
-if [ -z "$venv_dir" ]; then
-  echo "Please provide the target venv directory on the command line." >&2
+if [ -z "${venv_dir}" ]
+then
+  echo "Please provide the target venv directory on command line." >&2
   exit 1
 fi
 
-PYTHON=python3.11
-VENV="$venv_dir"
-PIP="$VENV/bin/pip"
+PYTHON=python3
+VENV=${venv_dir}
+PIP=${VENV}/bin/pip
 
-# Create a virtual environment, upgrade pip, and install requirements
-$PYTHON -m venv "$VENV"
-$PIP install --upgrade pip
-$PIP install -r "${root_dir}/requirements.txt"
+${PYTHON} -m venv "${VENV}"
+${PIP} install --upgrade pip
+${PIP} install --requirement "${root_dir}/requirements.txt"
